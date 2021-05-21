88c6852927d1276e2456d0e492d70fd479975708
ngboonhai <boon.hai.ng@intel.com>
2021-05-21 17:11:53 +0800

    Update Anaconda3_install.sh

--- cm/mpoc/Intel/Anaconda3_install.sh
+++ cm/mpoc/Intel/Anaconda3_install.sh
@@ -1,4 +1,4 @@
 wget https://repo.anaconda.com/archive/Anaconda3-5.0.0-Linux-x86_64.sh
 bash Anaconda3-5.0.0-Linux-x86_64.sh -b -f -p /opt/anaconda3
-export PATH=/opt/anaconda3/bin:$PATH
-source ~/.bashrc
+echo "PATH=/opt/anaconda3/bin:$PATH" > /etc/environment
+source /etc/environment
