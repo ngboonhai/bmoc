84ce5af8987a6756033ea70342fa5670e8ee1357
ngboonhai <boon.hai.ng@intel.com>
2021-05-22 17:03:40 +0800

    Update Anaconda3_install.sh

--- cm/mpoc/Intel/Anaconda3_install.sh
+++ cm/mpoc/Intel/Anaconda3_install.sh
@@ -1,4 +1,9 @@
-wget https://repo.anaconda.com/archive/Anaconda3-5.0.0-Linux-x86_64.sh
+os=uname
+arc=lscpu | grep Arch | awk '{print $2}'
+os_version=$os-$arc
+echo $os_version
+
+wget https://repo.anaconda.com/archive/Anaconda3-5.0.0-$os_version.sh
 bash Anaconda3-5.0.0-Linux-x86_64.sh -b -f -p /opt/anaconda3
 echo "PATH=/opt/anaconda3/bin:$PATH" > /etc/environment
 printf 'Please run the command: "source /etc/environment" to enable anaconda utils on environment \n'
