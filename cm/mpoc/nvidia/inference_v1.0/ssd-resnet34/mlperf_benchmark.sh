dcfc14ab9bb835ebc977f1d2d09d8645d96fd24b
ngboonhai <boon.hai.ng@intel.com>
2021-06-08 21:59:16 +0800

    Update mlperf_benchmark.sh

--- cm/mpoc/nvidia/inference_v1.0/ssd-resnet34/mlperf_benchmark.sh
+++ cm/mpoc/nvidia/inference_v1.0/ssd-resnet34/mlperf_benchmark.sh
@@ -30,7 +30,7 @@ bash $INFERENCE_NVIDIA_PATH/code/ssd-resnet34/tensorrt/download_model.sh
 
 
 ## Validate and Calibrate Models format and Images
-python3 $INFERENCE_NVIDIA_PATH/code/ssd-resnet34/preprocess_data.py
+python3 $INFERENCE_NVIDIA_PATH/code/ssd-resnet34/tensorrt/preprocess_data.py
 
 
 ## Execute MLPerf Benchmark
