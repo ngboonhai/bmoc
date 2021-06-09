2a1725edb1ff92ddf033a99cc8740c13a0cd2ae4
ngboonhai <boon.hai.ng@intel.com>
2021-06-09 22:51:14 +0800

    Create mlperf_benchmark.sh

---
+++ cm/mpoc/nvidia/inference_v0.7/ssd-mobilenet/mlperf_benchmark.sh
@@ -0,0 +1,46 @@
+#set -eo pipefail
+set -x
+
+git clone https://github.com/mlcommons/inference_results_v0.7.git ~/inference_results_v0.7
+INFERENCE_NVIDIA_PATH=~/inference_results_v0.7/closed/NVIDIA
+MLPERF_SCRATCH_PATH=$INFERENCE_NVIDIA_PATH/build
+mkdir -p $MLPERF_SCRATCH_PATH
+echo "export INFERENCE_NVIDIA_PATH=$INFERENCE_NVIDIA_PATH" >> ~/.bashrc
+echo "export MLPERF_SCRATCH_PATH=$MLPERF_SCRATCH_PATH" >> ~/.bashrc
+source ~/.bashrc
+export | grep $INFERENCE_NVIDIA_PATH
+
+## Update some files which errors detect from Origical files from Repo
+cat bmoc/cm/mpoc/nvidia/inference_v0.7/install_xavier_dependencies.sh > $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
+cat bmoc/cm/mpoc/nvidia/inference_v0.7/Makefile > $INFERENCE_NVIDIA_PATH/Makefile
+
+
+## Dependencies only for Jetson system
+bash $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
+
+## Build TensorRT and MLPerf Plugins
+cd $INFERENCE_NVIDIA_PATH
+make clone_loadgen
+# make build_plugins
+make build_loadgen
+make build_harness
+
+## Perform dataset download.
+bash $INFERENCE_NVIDIA_PATH/code/ssd-mobilenet/tensorrt/download_data.sh
+
+
+## Download Onnx Model from Zenodo Org.
+bash $INFERENCE_NVIDIA_PATH/code/ssd-mobilenet/tensorrt/download_model.sh
+
+
+## Validate and Calibrate Models format and Images
+python3 $INFERENCE_NVIDIA_PATH/code/ssd-mobilenet/tensorrt/preprocess_data.py
+
+
+## Execute MLPerf Benchmark
+# make run RUN_ARGS="--benchmarks=ssd-mobilenet --scenarios=SingleStream --test_mode=PerformanceOnly"
+# make run RUN_ARGS="--benchmarks=ssd-mobilenet --scenarios=SingleStream --test_mode=AccuracyOnly"
+# make run RUN_ARGS="--benchmarks=ssd-mobilenet --scenarios=MultiStream --test_mode=PerformanceOnly"
+# make run RUN_ARGS="--benchmarks=ssd-mobilenet --scenarios=MultiStream --test_mode=AccuracyOnly"
+# make run RUN_ARGS="--benchmarks=ssd-mobilenet --scenarios=Offline --test_mode=PerformanceOnly"
+# make run RUN_ARGS="--benchmarks=ssd-mobilenet --scenarios=Offline --test_mode=AccuracyOnly"
