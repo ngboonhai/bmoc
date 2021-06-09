679261b4226e3f52b4b67142294e8fe41b637bdf
ngboonhai <boon.hai.ng@intel.com>
2021-06-09 19:44:25 +0800

    Update mlperf_benchmark.sh

--- cm/mpoc/nvidia/inference_v0.7/resnet50/mlperf_benchmark.sh
+++ cm/mpoc/nvidia/inference_v0.7/resnet50/mlperf_benchmark.sh
@@ -13,8 +13,8 @@ export | grep $INFERENCE_NVIDIA_PATH
 
 ## Update some files which errors detect from Origical files from Repo
 https://github.com/ngboonhai/bmoc 
-cat bmoc/cm/mpoc/nvidia/inference_v0.7/resnet50/install_xavier_dependencies.sh > $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
-cat bmoc/cm/mpoc/nvidia/inference_v0.7/resnet50/Makefile > $INFERENCE_NVIDIA_PATH/Makefile
+cat bmoc/cm/mpoc/nvidia/inference_v0.7/install_xavier_dependencies.sh > $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
+cat bmoc/cm/mpoc/nvidia/inference_v0.7/Makefile > $INFERENCE_NVIDIA_PATH/Makefile
 
 ## Dependencies only for Jetson system
 bash $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
