0ece07e2384915635b0ecdbc47379c88ec803425
ngboonhai <boon.hai.ng@intel.com>
2021-06-09 22:45:07 +0800

    Update mlperf_benchmark.sh

--- cm/mpoc/nvidia/inference_v1.0/3d-unet/mlperf_benchmark.sh
+++ cm/mpoc/nvidia/inference_v1.0/3d-unet/mlperf_benchmark.sh
@@ -12,7 +12,6 @@ export | grep $INFERENCE_NVIDIA_PATH
 
 
 ## Update some files which errors detect from Origical files from Repo
-https://github.com/ngboonhai/bmoc.git
 cat bmoc/cm/mpoc/nvidia/inference_v1.0/install_xavier_dependencies.sh > $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
 cat bmoc/cm/mpoc/nvidia/inference_v1.0/Makefile > $INFERENCE_NVIDIA_PATH/Makefile
 
