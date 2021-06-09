2ace8eee4cb55d8ab13a43580bb9e7958b09036d
ngboonhai <boon.hai.ng@intel.com>
2021-06-09 19:53:26 +0800

    Update mlperf_benchmark.sh

--- cm/mpoc/nvidia/inference_v1.0/ssd-mobilenet/mlperf_benchmark.sh
+++ cm/mpoc/nvidia/inference_v1.0/ssd-mobilenet/mlperf_benchmark.sh
@@ -10,6 +10,12 @@ echo "export MLPERF_SCRATCH_PATH=$MLPERF_SCRATCH_PATH" >> ~/.bashrc
 source ~/.bashrc
 export | grep $INFERENCE_NVIDIA_PATH
 
+## Update some files which errors detect from Origical files from Repo
+https://github.com/ngboonhai/bmoc 
+cat bmoc/cm/mpoc/nvidia/inference_v1.0/install_xavier_dependencies.sh > $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
+cat bmoc/cm/mpoc/nvidia/inference_v1.0/Makefile > $INFERENCE_NVIDIA_PATH/Makefile
+
+
 ## Dependencies only for Jetson system
 bash $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
 
