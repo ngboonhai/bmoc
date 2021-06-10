56ffeb4ef7cb4172ffbdb03ea52f62f31cd939ff
ngboonhai <boon.hai.ng@intel.com>
2021-06-10 12:34:19 +0800

    Update mlperf_benchmark.sh

--- cm/mpoc/nvidia/inference_v0.7/ssd-resnet34/mlperf_benchmark.sh
+++ cm/mpoc/nvidia/inference_v0.7/ssd-resnet34/mlperf_benchmark.sh
@@ -18,6 +18,8 @@ cat bmoc/cm/mpoc/nvidia/inference_v0.7/lwis_buffers.h > $INFERENCE_NVIDIA_PATH/c
 
 
 ## Dependencies only for Jetson system
+sudo apt-get update
+sudo apt-get install libopenmpi2
 bash $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
 
 ## Build TensorRT and MLPerf Plugins
