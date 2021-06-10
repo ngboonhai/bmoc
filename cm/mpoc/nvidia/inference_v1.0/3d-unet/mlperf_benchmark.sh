32fdbe257e5ef9a47f1f7ae23126a588be849805
ngboonhai <boon.hai.ng@intel.com>
2021-06-10 18:09:29 +0800

    Update mlperf_benchmark.sh

--- cm/mpoc/nvidia/inference_v1.0/3d-unet/mlperf_benchmark.sh
+++ cm/mpoc/nvidia/inference_v1.0/3d-unet/mlperf_benchmark.sh
@@ -38,14 +38,17 @@ unzip MICCAI_BraTS_2019_Data_Training.zip -d $MLPERF_SCRATCH_PATH/data/BraTS
 
 
 ## Perform dataset validation after downloaded.
+cd $INFERENCE_NVIDIA_PATH
 bash $INFERENCE_NVIDIA_PATH/code/3d-unet/tensorrt/download_data.sh
 
 
 ## Download Onnx Model from Zenodo Org.
+cd $INFERENCE_NVIDIA_PATH
 bash $INFERENCE_NVIDIA_PATH/code/3d-unet/tensorrt/download_model.sh
 
 
 ## Validate and Calibrate Models format and Images
+cd $INFERENCE_NVIDIA_PATH
 python3 $INFERENCE_NVIDIA_PATH/code/3d-unet/tensorrt/preprocess_data.py
 
 
