9063126f2af2eeff3ac0cf7642ee8d9183cda124
ngboonhai <boon.hai.ng@intel.com>
2021-06-11 14:27:43 +0800

    Create install_dependencies.sh

---
+++ cm/mpoc/nvidia/inference_v1.0/3d-unet/install_dependencies.sh
@@ -0,0 +1,29 @@
+#set -eo pipefail
+set -x
+
+## Variable declaration
+MLPERF_INFERENCE_REPO="inference_results_v1.0"
+INFERENCE_NVIDIA_PATH="~/inference_results_v1.0/closed/NVIDIA"
+MLPERF_SCRATCH_PATH="$INFERENCE_NVIDIA_PATH/build"
+
+## Checkout MLPerf Inference v1.0 repo from GitHub
+[ ! -d "$MLPERF_INFERENCE_REPO" ] && git clone https://github.com/mlcommons/$MLPERF_INFERENCE_REPO.git ~/$MLPERF_INFERENCE_REPO
+
+## Create NVIDIA MLPerf scratch path
+[ ! -d "$MLPERF_SCRATCH_PATH" ] $$ mkdir -p $MLPERF_SCRATCH_PATH
+
+## Set NVIDIA Mlperf scratch path as envrionment variable
+grep -Rn "$MLPERF_SCRATCH_PATH" ~/.bashrc
+[ "$?" -ne "0" ] && echo "export INFERENCE_NVIDIA_PATH=$INFERENCE_NVIDIA_PATH" >> ~/.bashrc && echo "export MLPERF_SCRATCH_PATH=$MLPERF_SCRATCH_PATH" >> ~/.bashrc
+source ~/.bashrc
+export | grep $INFERENCE_NVIDIA_PATH
+
+## Update some files which errors detect from Origical files from Repo
+cat bmoc/cm/mpoc/nvidia/inference_v1.0/install_xavier_dependencies.sh > $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
+cat bmoc/cm/mpoc/nvidia/inference_v1.0/Makefile > $INFERENCE_NVIDIA_PATH/Makefile
+cat bmoc/cm/mpoc/nvidia/inference_v1.0/lwis_buffers.h > $INFERENCE_NVIDIA_PATH/code/harness/lwis/include/lwis_buffers.h
+
+
+## Dependencies only for Jetson system
+sudo apt-get update
+bash $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
