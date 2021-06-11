adbe5478b4d2910e1f885bc683e8ac2a075230f7
ngboonhai <boon.hai.ng@intel.com>
2021-06-11 17:14:54 +0800

    Update install_dependencies.sh

--- cm/mpoc/nvidia/inference_v1.0/ssd-resnet34/install_dependencies.sh
+++ cm/mpoc/nvidia/inference_v1.0/ssd-resnet34/install_dependencies.sh
@@ -13,14 +13,13 @@ MLPERF_SCRATCH_PATH=$INFERENCE_NVIDIA_PATH/build
 [ ! -d "$MLPERF_SCRATCH_PATH" ] && mkdir -p $MLPERF_SCRATCH_PATH
 
 ## Set NVIDIA Mlperf scratch path as envrionment variable
-grep -Rn "$MLPERF_SCRATCH_PATH" ~/.bashrc
-[ "$?" -ne "0" ] && echo "export INFERENCE_NVIDIA_PATH=$INFERENCE_NVIDIA_PATH" >> ~/.bashrc && echo "export MLPERF_SCRATCH_PATH=$MLPERF_SCRATCH_PATH" >> ~/.bashrc
-source ~/.bashrc
+## Check and Set NVIDIA Mlperf scratch path as envrionment variable
+[[ ! -z `export | grep INFERENCE_NVIDIA_PATH` ]] && echo $INFERENCE_NVIDIA_PATH || export INFERENCE_NVIDIA_PATH=~/inference_results_v1.0/closed/NVIDIA
+[[ ! -z `export | grep MLPERF_SCRATCH_PATH` ]] && echo $MLPERF_SCRATCH_PATH || export MLPERF_SCRATCH_PATH=$INFERENCE_NVIDIA_PATH/build
 export | grep $INFERENCE_NVIDIA_PATH
 
 ## Update some files which errors detect from Origical files from Repo
 cat bmoc/cm/mpoc/nvidia/inference_v1.0/install_xavier_dependencies.sh > $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
-
 
 ## Dependencies only for Jetson system
 sudo apt-get update
