e2147a1c2149ef57cfa7a2baa13e86f42c4fb4bc
ngboonhai <boon.hai.ng@intel.com>
2021-06-11 17:20:31 +0800

    Update mlperf_benchmark.sh

--- cm/mpoc/nvidia/inference_v1.0/ssd-resnet34/mlperf_benchmark.sh
+++ cm/mpoc/nvidia/inference_v1.0/ssd-resnet34/mlperf_benchmark.sh
@@ -1,12 +1,19 @@
 #set -eo pipefail
 set -x
 
-git clone https://github.com/mlcommons/inference_results_v1.0.git ~/inference_results_v1.0
+## Variable declaration
+MLPERF_INFERENCE_REPO="inference_results_v1.0"
 INFERENCE_NVIDIA_PATH=~/inference_results_v1.0/closed/NVIDIA
 MLPERF_SCRATCH_PATH=$INFERENCE_NVIDIA_PATH/build
-mkdir -p $MLPERF_SCRATCH_PATH
-echo "export INFERENCE_NVIDIA_PATH=$INFERENCE_NVIDIA_PATH" >> ~/.bashrc
-echo "export MLPERF_SCRATCH_PATH=$MLPERF_SCRATCH_PATH" >> ~/.bashrc
+
+## Checkout MLPerf Inference v1.0 repo from GitHub
+[ ! -d "$MLPERF_INFERENCE_REPO" ] && git clone https://github.com/mlcommons/$MLPERF_INFERENCE_REPO.git ~/$MLPERF_INFERENCE_REPO
+
+## Create NVIDIA MLPerf scratch path
+[ ! -d "$MLPERF_SCRATCH_PATH" ] && mkdir -p $MLPERF_SCRATCH_PATH
+
+grep -Rn "$MLPERF_SCRATCH_PATH" ~/.bashrc
+[ "$?" -ne "0" ] && echo "export INFERENCE_NVIDIA_PATH=$INFERENCE_NVIDIA_PATH" >> ~/.bashrc && echo "export MLPERF_SCRATCH_PATH=$MLPERF_SCRATCH_PATH" >> ~/.bashrc
 source ~/.bashrc
 export | grep $INFERENCE_NVIDIA_PATH
 
@@ -20,11 +27,12 @@ cat bmoc/cm/mpoc/nvidia/inference_v1.0/lwis_buffers.h > $INFERENCE_NVIDIA_PATH/c
 ## Dependencies only for Jetson system
 sudo apt-get update
 sudo apt-get install -y libopenmpi2
+pip install absl-py
 bash $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
 
 ## Build TensorRT and MLPerf Plugins
 cd $INFERENCE_NVIDIA_PATH
-make clone_loadgen
+[ ! -d "$MLPERF_SCRATCH_PATH/inference" ] && make clone_loadgen
 make build_plugins
 make build_loadgen
 make build_harness
