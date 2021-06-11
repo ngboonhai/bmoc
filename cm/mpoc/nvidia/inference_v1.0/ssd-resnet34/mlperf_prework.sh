82b7d66b034c3776d6f35a42eb85d4aebdf6259a
ubuntu <boonhai78@gmail.com>
2021-06-11 17:13:35 +0800

    update script

--- cm/mpoc/nvidia/inference_v1.0/ssd-resnet34/mlperf_prework.sh
+++ cm/mpoc/nvidia/inference_v1.0/ssd-resnet34/mlperf_prework.sh
@@ -13,28 +13,22 @@ MLPERF_SCRATCH_PATH=$INFERENCE_NVIDIA_PATH/build
 [ ! -d "$MLPERF_SCRATCH_PATH" ] && mkdir -p $MLPERF_SCRATCH_PATH
 
 ## Check and Set NVIDIA Mlperf scratch path as envrionment variable
-[[ ! -z $INFERENCE_NVIDIA_PATH ]] && echo $INFERENCE_NVIDIA_PATH || export INFERENCE_NVIDIA_PATH=~/inference_results_v1.0/closed/NVIDIA
-[[ ! -z $MLPERF_SCRATCH_PATH ]] && echo $MLPERF_SCRATCH_PATH || export MLPERF_SCRATCH_PATH=$INFERENCE_NVIDIA_PATH/build
-# grep -Rn "$MLPERF_SCRATCH_PATH" ~/.bashrc
-# [ "$?" -ne "0" ] && echo "export INFERENCE_NVIDIA_PATH=$INFERENCE_NVIDIA_PATH" >> ~/.bashrc && echo "export MLPERF_SCRATCH_PATH=$MLPERF_SCRATCH_PATH" >> ~/.bashrc
-# source ~/.bashrc
-# export | grep $INFERENCE_NVIDIA_PATH
+[[ ! -z `export | grep INFERENCE_NVIDIA_PATH` ]] && echo $INFERENCE_NVIDIA_PATH || export INFERENCE_NVIDIA_PATH=~/inference_results_v1.0/closed/NVIDIA
+[[ ! -z `export | grep MLPERF_SCRATCH_PATH` ]] && echo $MLPERF_SCRATCH_PATH || export MLPERF_SCRATCH_PATH=$INFERENCE_NVIDIA_PATH/build
+export | grep $INFERENCE_NVIDIA_PATH
 
 
 ## Perform dataset download.
 cd $INFERENCE_NVIDIA_PATH
-id
 bash $INFERENCE_NVIDIA_PATH/code/ssd-mobilenet/tensorrt/download_data.sh
 
 
 ## Download Onnx Model from Zenodo Org.
 cd $INFERENCE_NVIDIA_PATH
-pwd
 bash $INFERENCE_NVIDIA_PATH/code/ssd-mobilenet/tensorrt/download_model.sh
 
 
 ## Validate and Calibrate Models format and Images
 cd $INFERENCE_NVIDIA_PATH
-pwd
 python3 $INFERENCE_NVIDIA_PATH/code/ssd-mobilenet/tensorrt/preprocess_data.py --cal_only
 python3 $INFERENCE_NVIDIA_PATH/code/ssd-mobilenet/tensorrt/preprocess_data.py
