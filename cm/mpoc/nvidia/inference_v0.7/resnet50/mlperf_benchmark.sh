766c9f2ab926e7f39c185c22bc13bf56a29aa7d1
ngboonhai <boon.hai.ng@intel.com>
2021-06-11 12:25:04 +0800

    Update mlperf_benchmark.sh

--- cm/mpoc/nvidia/inference_v0.7/resnet50/mlperf_benchmark.sh
+++ cm/mpoc/nvidia/inference_v0.7/resnet50/mlperf_benchmark.sh
@@ -53,6 +53,7 @@ python3 $INFERENCE_NVIDIA_PATH/code/resnet50/tensorrt/preprocess_data.py
 
 
 ## Execute MLPerf Benchmark
+cd $INFERENCE_NVIDIA_PATH
 make run RUN_ARGS="--benchmarks=resnet50 --scenarios=SingleStream --test_mode=PerformanceOnly"
 make run RUN_ARGS="--benchmarks=resnet50 --scenarios=SingleStream --test_mode=AccuracyOnly"
 # make run RUN_ARGS="--benchmarks=resnet50 --scenarios=MultiStream --test_mode=PerformanceOnly"
