e4e6b7074a7b799491ac9a1f853f1dd9b9201f16
ngboonhai <boon.hai.ng@intel.com>
2021-06-11 21:36:44 +0800

    Update mlperf_benchmark.sh

--- cm/mpoc/nvidia/inference_v1.0/ssd-resnet34/mlperf_benchmark.sh
+++ cm/mpoc/nvidia/inference_v1.0/ssd-resnet34/mlperf_benchmark.sh
@@ -39,19 +39,26 @@ make build_loadgen
 make build_harness
 
 
+## Check and Set NVIDIA Mlperf scratch path as envrionment variable
+[[ ! -z `export | grep INFERENCE_NVIDIA_PATH` ]] && echo $INFERENCE_NVIDIA_PATH || export INFERENCE_NVIDIA_PATH=~/inference_results_v1.0/closed/NVIDIA
+[[ ! -z `export | grep MLPERF_SCRATCH_PATH` ]] && echo $MLPERF_SCRATCH_PATH || export MLPERF_SCRATCH_PATH=$INFERENCE_NVIDIA_PATH/build
+export | grep $INFERENCE_NVIDIA_PATH
+
+
 ## Perform dataset download.
 cd $INFERENCE_NVIDIA_PATH
-bash $INFERENCE_NVIDIA_PATH/code/ssd-resnet34/tensorrt/download_data.sh
+bash $INFERENCE_NVIDIA_PATH/code/ssd-mobilenet/tensorrt/download_data.sh
 
 
 ## Download Onnx Model from Zenodo Org.
 cd $INFERENCE_NVIDIA_PATH
-bash $INFERENCE_NVIDIA_PATH/code/ssd-resnet34/tensorrt/download_model.sh
+bash $INFERENCE_NVIDIA_PATH/code/ssd-mobilenet/tensorrt/download_model.sh
 
 
 ## Validate and Calibrate Models format and Images
 cd $INFERENCE_NVIDIA_PATH
-python3 $INFERENCE_NVIDIA_PATH/code/ssd-resnet34/tensorrt/preprocess_data.py
+python3 $INFERENCE_NVIDIA_PATH/code/ssd-mobilenet/tensorrt/preprocess_data.py --cal_only
+python3 $INFERENCE_NVIDIA_PATH/code/ssd-mobilenet/tensorrt/preprocess_data.py
 
 
 ## Execute MLPerf Benchmark
