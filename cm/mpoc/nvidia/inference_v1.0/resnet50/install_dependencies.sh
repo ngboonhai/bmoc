eaa65424bc820bd3f457dc4bb37cae91a12503ec
ngboonhai <boon.hai.ng@intel.com>
2021-06-13 15:27:59 +0800

    Update install_dependencies.sh

--- cm/mpoc/nvidia/inference_v1.0/resnet50/install_dependencies.sh
+++ cm/mpoc/nvidia/inference_v1.0/resnet50/install_dependencies.sh
@@ -23,7 +23,7 @@ cat bmoc/cm/mpoc/nvidia/inference_v1.0/install_xavier_dependencies.sh > $INFEREN
 
 ## Dependencies only for Jetson system
 sudo apt-get update
-sudo apt-get install -y curl
+sudo apt-get install -y python-dev python3-dev python-pip python3-pip curl libopenmpi2
 pip3 install scikit-build
 pip install absl-py
 bash $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
