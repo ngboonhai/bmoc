16c8d0ca731113356e61a728646f2a9e21ab5f9c
ngboonhai <boon.hai.ng@intel.com>
2021-06-13 21:38:03 +0800

    Update mlperf_benchmark.sh

--- cm/mpoc/nvidia/inference_v0.7/3d-unet/mlperf_benchmark.sh
+++ cm/mpoc/nvidia/inference_v0.7/3d-unet/mlperf_benchmark.sh
@@ -32,7 +32,8 @@ cat bmoc/cm/mpoc/nvidia/inference_v0.7/lwis_buffers.h > $INFERENCE_NVIDIA_PATH/c
 ## Dependencies only for Jetson system
 sudo apt-get update
 sudo apt-get install -y python-dev python3-dev python-pip python3-pip curl libopenmpi2
-pip3 install scikit-build onnx
+pip3 install scikit-build
+pip install absl-py
 pip3 install git+https://github.com/SimpleITK/SimpleITKPythonPackage.git -v
 bash $INFERENCE_NVIDIA_PATH/scripts/install_xavier_dependencies.sh
 
