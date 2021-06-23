ba70994499d986c6c0951f435a2e32b3bc96c0b6
ngboonhai <boon.hai.ng@intel.com>
2021-06-23 12:37:06 +0800

    Update mlperf_benchmark.sh

--- cm/mpoc/nvidia/inference_v0.7/3d-unet/mlperf_benchmark.sh
+++ cm/mpoc/nvidia/inference_v0.7/3d-unet/mlperf_benchmark.sh
@@ -92,5 +92,5 @@ python3 $INFERENCE_NVIDIA_PATH/code/3d-unet/tensorrt/preprocess_data.py
 ## Execute MLPerf Benchmark
 export PREPROCESSED_DATA_DIR="build/preprocessed_data"
 cd $INFERENCE_NVIDIA_PATH
-make run RUN_ARGS="--benchmarks=3d-unet --scenarios=SingleStream --test_mode=PerformanceOnly"
-make run RUN_ARGS="--benchmarks=3d-unet --scenarios=Offline --test_mode=PerformanceOnly"
+make run RUN_ARGS="--benchmarks=3d-unet --scenarios=SingleStream --config_ver=default --test_mode=PerformanceOnly"
+make run RUN_ARGS="--benchmarks=3d-unet --scenarios=Offline --config_ver=default --test_mode=PerformanceOnly"
