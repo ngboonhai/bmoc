bd88d82847e0de0f7d3a5b77496a024041ce03dc
bng9 <boon.hai.ng@intel.com>
2021-06-27 20:08:17 +0800

    update ssd-resnet34 mlperf code

--- cm/mpoc/intel/inference_v1.0/ssd-resnet34/ssd-resnet34_mlperf_benchmark.sh
+++ cm/mpoc/intel/inference_v1.0/ssd-resnet34/ssd-resnet34_mlperf_benchmark.sh
@@ -13,27 +13,27 @@ else
 fi
 
 
-##  Check and ready Resnet50 MLPerf configuration files.
-if [ ! -d ${CUR_DIR}/Configs/resnet50 ]; then
-    mkdir -p ${CUR_DIR}/Configs/resnet50
-    cp -r ${CUR_DIR}/bmoc/cm/mpoc/intel/inference_v1.0/resnet50/config/* ${CUR_DIR}/Configs/resnet50/
-    echo -e "\e[0;32m Copied Resnet50 mlperf configudation to current working directory!!\e[0m"
+##  Check and ready ssd-resnet34 MLPerf configuration files.
+if [ ! -d ${CUR_DIR}/Configs/ssd-resnet34 ]; then
+    mkdir -p ${CUR_DIR}/Configs/ssd-resnet34
+    cp -r ${CUR_DIR}/bmoc/cm/mpoc/intel/inference_v1.0/ssd-resnet34/config/* ${CUR_DIR}/Configs/ssd-resnet34/
+    echo -e "\e[0;32m Copied ssd-resnet34 mlperf configudation to current working directory!!\e[0m"
 else
-    echo -e "\e[0;32m Existing Resnet50 mlperf configudation detected!!\e[0m"
+    echo -e "\e[0;32m Existing ssd-resnet34 mlperf configudation detected!!\e[0m"
 fi
 
 ## Sanity check all the pre-work is ready before run mlperf benchmark task
-if [ -d ${CUR_DIR}/datasets/resnet50/dataset-imagenet-ilsvrc2012-val ]; then
-    echo -e "\e[0;32m Resnet50 imagenet datasets is ready!!\e[0m"
+if [ -d ${CUR_DIR}/datasets/ssd-resnet34/dataset-coco-2017-val ]; then
+    echo -e "\e[0;32m ssd-resnet34 imagenet datasets is ready!!\e[0m"
 else
-    echo -e "\e[0;31m Unable to find resnet50 imagenet datasets, please check!!\e[0m"
+    echo -e "\e[0;31m Unable to find ssd-resnet34 imagenet datasets, please check!!\e[0m"
     exit 1
 fi
 
-if [ -f ${CUR_DIR}/models/resnet50/resnet50_fp16.xml ]; then
-    echo -e "\e[0;32m Resnet50 IR files is ready!!\e[0m"
+if [ -f ${CUR_DIR}/models/ssd-resnet34/ssd-resnet34_fp16.xml ]; then
+    echo -e "\e[0;32m ssd-resnet34 IR files is ready!!\e[0m"
 else
-    echo -e "\e[0;31m Unable to find resnet50 IR file, please check!!\e[0m"
+    echo -e "\e[0;31m Unable to find ssd-resnet34 IR file, please check!!\e[0m"
     exit 1
 fi
 
@@ -45,5 +45,5 @@ else}
     SCENARIO=${SCENARIO}
 fi
 
-## Run MLPerf benchmark for Resnet50 model
-bash run_mlperf.sh Configs/resnet50/${SCENARIO}-config.json
+## Run MLPerf benchmark for ssd-resnet34 model
+bash run_mlperf.sh Configs/ssd-resnet34/${SCENARIO}-config.json
