a080efc5571abb4e8499a5cf3e5a878a145cebc1
bng9 <boon.hai.ng@intel.com>
2021-06-27 15:47:38 +0800

    update resnet50 automation script

--- cm/mpoc/intel/inference_v1.0/resnet50/resnet50_mlperf_benchmark.sh
+++ cm/mpoc/intel/inference_v1.0/resnet50/resnet50_mlperf_benchmark.sh
@@ -7,8 +7,31 @@ CUR_DIR=`pwd`
 if [ ! -d ${CUR_DIR}/Configs/resnet50 ]; then
 	mkdir -p ${CUR_DIR}/Configs/resnet50
 	cp -r ${CUR_DIR}/bmoc/cm/mpoc/intel/inference_v1.0/resnet50/config/* ${CUR_DIR}/Configs/resnet50/
-	echo -e "\e[0;31m Copied Resnet50 mlperf configudation to current working directory!!\e[0m"
+	echo -e "\e[0;32m Copied Resnet50 mlperf configudation to current working directory!!\e[0m"
 else
 	echo -e "\e[0;31m Existing Resnet50 mlperf configudation detected!!\e[0m"
 fi
 
+## Sanity check all the pre-work is ready before run mlperf benchmark task
+if [ ! -f ${CUR_DIR}/datasets/resnet50/dataset-imagenet-ilsvrc2012-val/ ]: then
+	echo -e "\e[0;32m Resnet50 imagenet datasets is ready!!\e[0m"
+else
+	echo -e "\e[0;31m Unable to find resnet50 imagenet datasets, please check!!\e[0m"
+fi
+
+if [ ! -f ${CUR_DIR}/models/resnet50/resnet50_fp16.xml ]; then
+	echo -e "\e[0;32m Resnet50 IR files is ready!!\e[0m"
+else
+	echo -e "\e[0;31m Unable to find resnet50 IR file, please check!!\e[0m"
+fi
+
+## Config mlperf benchmark scenario and Test mode default values
+SCENARIO=$1
+if [ "${SCENARIO}" == "" ]; then
+    SCENARIO="SingleStream"
+else}
+    SCENARIO=${SCENARIO}
+fi
+
+## Run MLPerf benchmark for Resnet50 model
+bash run_mlperf.sh Configs/resnet50/${SCENARIO}-config.json
