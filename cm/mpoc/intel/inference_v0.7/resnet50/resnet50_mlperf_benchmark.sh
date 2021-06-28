9f58554ef557bff4dcb080dee5e6fb0f67927299
ngboonhai <boon.hai.ng@intel.com>
2021-06-29 00:21:07 +0800

    Update resnet50_mlperf_benchmark.sh

--- cm/mpoc/intel/inference_v0.7/resnet50/resnet50_mlperf_benchmark.sh
+++ cm/mpoc/intel/inference_v0.7/resnet50/resnet50_mlperf_benchmark.sh
@@ -71,4 +71,4 @@ else
 fi
 
 ## Run MLPerf benchmark for Resnet50 model
-run_mlperf.sh Configs/resnet50/${SCENARIO}-${PRECISION}-config.json
+./run_mlperf.sh Configs/resnet50/${SCENARIO}-${PRECISION}-config.json
