27f002aa587e7099f5b4c14e57e00f2db3e3906e
ngboonhai <boon.hai.ng@intel.com>
2021-06-28 17:27:10 +0800

    Update install_dependecies.sh

--- cm/mpoc/intel/inference_v1.0/resnet50/install_dependecies.sh
+++ cm/mpoc/intel/inference_v1.0/resnet50/install_dependecies.sh
@@ -15,7 +15,7 @@ trap 'error ${LINENO}' ERR
 
 sudo apt update
 sudo apt-get install -y libglib2.0-dev libtbb-dev python3-dev python3-pip
-if [ ! `cmake --version | head -1 | awk '{print $3}'` -gt 3.10 ]; then
+if [ ! `cmake --version | head -1 | awk '{print $3}'` -gt 3.15 ]; then
 	wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
 	sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ $(. /etc/os-release && echo ${VERSION_CODENAME-stretch}) main'
 	sudo apt-get update
@@ -23,6 +23,8 @@ if [ ! `cmake --version | head -1 | awk '{print $3}'` -gt 3.10 ]; then
 	sudo rm /etc/apt/trusted.gpg.d/kitware.gpg
 	sudo apt-get update
 	sudo apt-get install -y cmake
+	cmake_version=`cmake --version | head -1 | awk '{print $3}'`
+	echo -e "\e[0;32m Cmake ${cmake_version} installed!!\e[0m"
 else
 	echo -e "\e[0;32m Cmake >=3.10 installed!!\e[0m"
 fi
