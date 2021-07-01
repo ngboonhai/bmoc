c669d98ffb3a2f595c300b1ce7eae410efd21b91
ngboonhai <boon.hai.ng@intel.com>
2021-07-01 17:52:29 +0800

    Update install_dependecies.sh

--- cm/mpoc/intel/inference_v1.0/3d-unet/install_dependecies.sh
+++ cm/mpoc/intel/inference_v1.0/3d-unet/install_dependecies.sh
@@ -52,6 +52,8 @@ if [ ! -d /opt/intel/openvino_2021 ]; then
 	cd ${CUR_DIR}
 	rm -rf l_openvino_toolkit_p_2021.3.394
 	rm l_openvino_toolkit_p_2021.3.394.tgz
+	ln -sf /opt/intel/openvino_2021.3.394 /opt/intel/openvino_2021
+	#source /opt/intel/openvino_2021/bin/setup_envs.sh
 	echo -e "\e[0;32m Cmake ${cmake_version} installed!!\e[0m"
 else
 	echo -e "\e[0;32m Cmake >=3.10 installed!!\e[0m"
