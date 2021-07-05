b8d39541d9d5aed9e8ee137326eeab5ff3819216
ngboonhai <boon.hai.ng@intel.com>
2021-07-05 11:55:53 +0800

    adding benchmark automation code

---
+++ cm/intel/benchmark_prework.sh
@@ -0,0 +1,82 @@
+#set -eo pipefail
+
+error() {
+    local code="${3:-1}"
+    if [[ -n "$2" ]];then
+        echo "Error on or near line $1: $2; exiting with status ${code}"
+    else
+        echo "Error on or near line $1; exiting with status ${code}"
+    fi
+    exit "${code}"
+}
+trap 'error ${LINENO}' ERR
+
+CUR_DIR=`pwd`
+BUILD_DIRECTORY=${CUR_DIR}
+SKIPS=" "
+DASHES="================================================"
+
+## Config mlperf benchmark scenario and Test mode default values
+MODEL=$1
+if [ "${MODEL}" == "" ]; then
+    echo -e "\e[0;31m [Error}: Model name is require for benchmarking !!! \e[0m"
+else
+    MODEL=${MODEL}
+fi
+
+PRECISION=$2
+if [ "${PRECISION}" == "" ]; then
+    PRECISION="fp16"
+else
+    PRECISION=${PRECISION}
+fi
+
+echo ${SKIPS}
+echo -e "\e[0;34m ========= Preparing benchmark configuration file ========= \e[0m"
+echo ${SKIPS}
+if [ ! -f ${CUR_DIR}/Configs/models_config.json ]; then
+	mkdir -p ${CUR_DIR}/Configs
+	cp bmoc/cm/intel/models_config.json ${CUR_DIR}/Configs/
+	echo -e "\e[0;32m ========== Copied benchmark configuration file =========== \e[0m"
+else
+	echo -e "\e[0;32m Existing benchmark configuration file detected!!\e[0m"
+fi
+
+echo ${SKIPS}
+echo -e "\e[0;34m ========= Downloading benchmark datasets ========= \e[0m"
+echo ${SKIPS}
+if [ ! -d /workload/benchmark/datasets/input_images ]; then
+	mkdir -p ${CUR_DIR}/datasets
+	wget https://ubit-artifactory-sh.intel.com/artifactory/esc-local/utils/input_images.zip
+	unzip -d ${CUR_DIR}/datasets input_images.zip
+	echo -e "\e[0;32m ========== Benchmark dataest download and extract completed =========== \e[0m"
+else
+	echo -e "\e[0;32m Existing benchmark dataests detected!!\e[0m"
+fi
+
+echo ${SKIPS}
+echo -e "\e[0;34m ========= Downloading benchmark models ========= \e[0m"
+echo ${SKIPS}
+if [ ! -d `find ${CUR_DIR} -type d -name "$1"  2>/dev/null` ]; then
+	mkdir -p ${CUR_DIR}/models
+	python3 /opt/intel/openvino_2021/deployment_tools/open_model_zoo/tools/downloader/downloader.py --name ${MODEL} -o ${CUR_DIR}/models/
+	MODEL_DIR=`find ${CUR_DIR} -type d -name "$1"  2>/dev/null`
+	echo -e "\e[0;32m ========== Benchmark models download and extract completed =========== \e[0m"
+else
+	MODEL_DIR=`find ${CUR_DIR} -type d -name "$1"  2>/dev/null`
+	echo -e "\e[0;32m Existing benchmark models detected!!\e[0m"
+fi
+
+echo ${SKIPS}
+echo -e "\e[0;34m ========= Optimizing benchmark models ========= \e[0m"
+echo ${SKIPS}
+if [ ! -f ${MODEL_DIR}/mobilenet-ssd_${PRECISION}.xml ]; then
+	MODEL_FILE=`jq -r '."'"${MODEL}"'"'.model_file ${CUR_DIR}/Configs/models_config.json`
+	FRAME_WORK=`jq -r '."'"${MODEL}"'"'.frame_work ${CUR_DIR}/Configs/models_config.json`
+	MODEL_FILE_PATH=`find /workload/benchmark -name $MODEL_FILE`
+	python3 /opt/intel/openvino_2021/deployment_tools/model_optimizer/mo_caffe.py --input_model ${MODEL_FILE_PATH} --data_type half --output_dir ${MODEL_DIR} --model_name mobilenet-ssd_${PRECISION}
+	echo -e "\e[0;32m ========== Benchmark models has been optimized and IR files generated =========== \e[0m"
+else
+	MODEL_DIR=`find ${CUR_DIR} -type d -name "$1"  2>/dev/null`
+	echo -e "\e[0;32m Existing benchmark models IR files detected!!\e[0m"
+fi
---
+++ cm/intel/install_dependencies.sh
@@ -0,0 +1,55 @@
+#set -eo pipefail
+
+error() {
+    local code="${3:-1}"
+    if [[ -n "$2" ]];then
+        echo "Error on or near line $1: $2; exiting with status ${code}"
+    else
+        echo "Error on or near line $1; exiting with status ${code}"
+    fi
+    exit "${code}"
+}
+trap 'error ${LINENO}' ERR
+
+CUR_DIR=`pwd`
+BUILD_DIRECTORY=${CUR_DIR}
+SKIPS=" "
+DASHES="================================================"
+
+echo ${SKIPS}
+echo -e "\e[0;34m ========= Check and installing workload dependencis ========= \e[0m"
+echo ${SKIPS}
+
+sudo apt-get update
+sudo apt install -y python3-opencv nvidia-cuda-toolkit jq
+sudo python3 -m pip install networkx defusedxml progress
+sudo chmod a+r /usr/lib/x86_64-linux-gnu/libcuda*
+
+if [ ! -d /opt/intel/openvino_2021 ]; then
+	echo ${SKIPS}
+	echo -e "\e[0;34m ========== Installing OpenVino Toolkit for Benchmark Tools=========== \e[0m"
+	echo ${SKIPS}
+	wget https://ubit-artifactory-sh.intel.com/artifactory/esc-local/utils/l_openvino_toolkit_p_2021.3.394.tgz
+	tar xvf l_openvino_toolkit_p_2021.3.394.tgz
+	cat ${CUR_DIR}/bmoc/cm/mpoc/intel/inference_v1.0/3d-unet/silent.cfg > l_openvino_toolkit_p_2021.3.394/silent.cfg
+	cd l_openvino_toolkit_p_2021.3.394
+	sudo ./install.sh -s silent.cfg
+	cd ${CUR_DIR}
+	rm -rf l_openvino_toolkit_p_2021.3.394*
+	sudo ln -sf /opt/intel/openvino_2021.3.394 /opt/intel/openvino
+	source /opt/intel/openvino_2021/bin/setupvars.sh
+	echo -e "\e[0;32m Cmake ${cmake_version} installed!!\e[0m"
+
+	echo -e "\e[0;34m ========== Installing OpenVino Toolkit Dependencies on system =========== \e[0m"
+	sudo bash -E /opt/intel/openvino/install_dependencies/install_openvino_dependencies.sh -y
+
+	echo -e "\e[0;34m ========== Installing OpenVino Toolkit Dependencies on system =========== \e[0m"
+	sudo bash -E /opt/intel/openvino/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh
+
+	echo -e "\e[0;32m ========== OpenVino Toolkit & Dependencies install completed =========== \e[0m"
+else
+	echo -e "\e[0;32m Existing OpenVino Toolkit & Dependencies detected!!\e[0m"
+fi
+
+
+echo -e "\e[0;32m ========== Dependencies install completed =========== \e[0m"
