2435afa06ad0f1f3581a3200ddf8de70b463cd8f
ngboonhai <boon.hai.ng@intel.com>
2021-07-07 17:11:31 +0800

    Update benchmark.sh

--- cm/intel/benchmark.sh
+++ cm/intel/benchmark.sh
@@ -46,8 +46,16 @@ IR_FILE_PATH=`find ${MODEL_DIR} -name "*.xml" 2>/dev/null`
 if [ ! "${IR_FILE_PATH}" == "" ]; then
         for file_path in `echo $IR_FILE_PATH`
         do
-                if [[ $file_path =~ "INT8" ]]; then
+                if [[ $file_path =~ "FP16-INT8" ]]; then
                         MODEL_FILE_PATH=$file_path
+                        FOUND="true"
+                        break
+		elif [[ $file_path =~ "FP16" ]]; then
+			MODEL_FILE_PATH=$file_path
+                        FOUND="true"
+                        break
+		elif [[ $file_path =~ "FP32" ]]; then
+			MODEL_FILE_PATH=$file_path
                         FOUND="true"
                         break
 		elif [[ $file_path =~ "fp16" ]]; then
