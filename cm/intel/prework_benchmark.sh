95fb69c373a44288f9392bf610dc797eca9b0842
ngboonhai <boon.hai.ng@intel.com>
2021-07-07 12:37:12 +0800

    Update prework_benchmark.sh

--- cm/intel/prework_benchmark.sh
+++ cm/intel/prework_benchmark.sh
@@ -114,6 +114,4 @@ else
 	python3 /opt/intel/openvino_2021/deployment_tools/model_optimizer/${FRAME_WORK_TOOL} --input_model ${MODEL_FILE_PATH} --data_type half --output_dir ${MODEL_DIR} --model_name ${MODEL}_${PRECISION}
 	echo ${SKIPS} 
 	echo -e "\e[0;32m ========== Benchmark models has been optimized and IR files generated =========== \e[0m"
-else
-	echo -e "\e[0;32m Existing benchmark models IR files detected!!\e[0m"
 fi
