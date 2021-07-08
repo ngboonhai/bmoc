6b083ac602df8a2352688733d5922be3395739cb
ngboonhai <boon.hai.ng@intel.com>
2021-07-09 00:07:00 +0800

    Update install_dependecies.sh

--- cm/mpoc/intel/inference_v1.0/ssd-resnet34/install_dependecies.sh
+++ cm/mpoc/intel/inference_v1.0/ssd-resnet34/install_dependecies.sh
@@ -66,12 +66,11 @@ else
 fi
 
 echo -e "\e[0;34m ========== continue Installing other(s) dependencies =========== \e[0m"
-python3 -m pip install networkx defusedxml numpy==1.16.4 test-generator==0.1.1 tensorflow==2.2.0rc1 onnx==1.7.0
 DIST=$(. /etc/os-release && echo ${VERSION_CODENAME-stretch})
 if [ "${DIST}" == "focal" ]; then
-        python3 -m pip install tensorflow==2.2.0rc1
+        python3 -m pip install networkx defusedxml numpy==1.16.4 test-generator==0.1.1 onnx==1.7.0 tensorflow==2.2.0rc1
 else
-        python3 -m pip install tensorflow==2.0.0a0
+        python3 -m pip install networkx defusedxml numpy==1.16.4 test-generator==0.1.1 onnx==1.7.0 tensorflow==2.0.0a0
 fi
 
 MLPERF_DIR=${BUILD_DIRECTORY}/MLPerf-Intel-openvino
