51d03f987eb33c5477b688159c59f1d794f2b5f4
ngboonhai <boon.hai.ng@intel.com>
2021-07-12 17:07:23 +0800

    Update install_dependencies.sh

--- cm/intel/install_dependencies.sh
+++ cm/intel/install_dependencies.sh
@@ -72,46 +72,49 @@ if [ "${DIST}" == "focal" ]; then
 	sudo python3 -m pip install networkx defusedxml progress numpy google protobuf
 	sudo python3 -m pip install requests --upgrade
 	sudo chmod a+r /usr/lib/x86_64-linux-gnu/libcuda*
+	SUDO="sudo"
 else
 	sudo apt-get install -y python-networkx python-defusedxml python-progress python-google-apputils python-protobuf python-numpy
 	python3 -m pip install test-generator tensorflow
 	python3 -m pip install requests --upgrade
 	sudo chmod a+r /usr/lib/x86_64-linux-gnu/libcuda*
+	SUDO=""
 fi
+
 
 echo ${SKIPS}
 echo -e "\e[0;34m ========== Installing OpenVino Model Optimizer Dependencies on system =========== \e[0m"
 echo ${SKIPS}
-sudo bash -E /opt/intel/openvino_2021/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh
+$SUDO bash -E /opt/intel/openvino_2021/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh
 
 echo ${SKIPS}
 echo -e "\e[0;34m ========== Installing OpenVino Model Optimizer Caffe Dependencies on system =========== \e[0m"
 echo ${SKIPS}
-sudo bash -E /opt/intel/openvino_2021/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh caffe
+$SUDO bash -E /opt/intel/openvino_2021/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh caffe
 
 echo ${SKIPS}
 echo -e "\e[0;34m ========== Installing OpenVino Model Optimizer ONNX Dependencies on system =========== \e[0m"
 echo ${SKIPS}
-sudo bash -E /opt/intel/openvino_2021/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh onnx
+$SUDO bash -E /opt/intel/openvino_2021/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh onnx
 
 echo ${SKIPS}
 echo -e "\e[0;34m ========== Installing OpenVino Model Optimizer TensorFlow Dependencies on system =========== \e[0m"
 echo ${SKIPS}
-sudo bash -E /opt/intel/openvino_2021/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh tf
+$SUDO bash -E /opt/intel/openvino_2021/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh tf
 
 echo ${SKIPS}
 echo -e "\e[0;34m ========== Installing OpenVino Model Optimizer TensorFlow v2 Dependencies on system =========== \e[0m"
 echo ${SKIPS}
-sudo bash -E /opt/intel/openvino_2021/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh tf2
+$SUDO bash -E /opt/intel/openvino_2021/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh tf2
 
 echo ${SKIPS}
 echo -e "\e[0;34m ========== Installing OpenVino Model Optimizer Keras Dependencies on system =========== \e[0m"
 echo ${SKIPS}
-sudo bash -E /opt/intel/openvino_2021/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh kaldi
+$SUDO bash -E /opt/intel/openvino_2021/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh kaldi
 
 echo ${SKIPS}
 echo -e "\e[0;34m ========== Installing OpenVino Model Optimizer MXNET Dependencies on system =========== \e[0m"
 echo ${SKIPS}
-sudo bash -E /opt/intel/openvino_2021/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh mxnet
+$SUDO bash -E /opt/intel/openvino_2021/deployment_tools/model_optimizer/install_prerequisites/install_prerequisites.sh mxnet
 
 echo -e "\e[0;32m ========== Dependencies install completed =========== \e[0m"
