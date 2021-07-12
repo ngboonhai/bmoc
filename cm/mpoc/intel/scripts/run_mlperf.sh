846605e6068f40470b3eb081520c9e387d20d436
ngboonhai <boon.hai.ng@intel.com>
2021-07-13 04:50:28 +0800

    Update run_mlperf.sh

--- cm/mpoc/intel/scripts/run_mlperf.sh
+++ cm/mpoc/intel/scripts/run_mlperf.sh
@@ -4,7 +4,7 @@ CONFIG_FILE="${1}"
 
 source setup_envs.sh
 
-echo " Config file: ${CONFIG_FILE}"
+echo -e "\e[0;34m Config file: ${CONFIG_FILE} \e[0m"
 
 if [ ${CONFIG_FILE} == "" ]; then
 	echo " No config file provided. Using default configuration"
