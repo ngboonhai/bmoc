d07417867187dc791db08b7f058276e5ee0b9d15
ngboonhai <boon.hai.ng@intel.com>
2021-07-12 16:54:14 +0800

    Update install_dependecies.sh

--- cm/mpoc/intel/inference_v1.0/3d-unet/install_dependecies.sh
+++ cm/mpoc/intel/inference_v1.0/3d-unet/install_dependecies.sh
@@ -19,7 +19,7 @@ echo -e "\e[0;34m ========= Check and installing workload dependencis ========= 
 echo ${SKIPS}
 
 sudo apt update
-sudo apt-get install -y libglib2.0-dev libtbb-dev python3-dev python3-pip python3-venv unzip python3.8-venv libssl-dev libgtk-3-dev
+sudo apt-get install -y libglib2.0-dev libtbb-dev python3-dev python3-pip python3-venv unzip python3.8-venv libssl-dev libgtk-3-dev shellcheck
 
 python3 -m venv 3d-unet
 source 3d-unet/bin/activate
