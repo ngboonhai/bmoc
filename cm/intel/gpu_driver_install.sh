5e1852973e4c85bfbe21944f9d37a370deb3d92d
ngboonhai <boon.hai.ng@intel.com>
2021-11-09 13:04:22 +0800

    Update gpu_driver_install.sh

--- cm/intel/gpu_driver_install.sh
+++ cm/intel/gpu_driver_install.sh
@@ -2,7 +2,7 @@ sudo apt-get install -y gpg-agent wget
 wget -qO - https://repositories.intel.com/graphics/intel-graphics.key | sudo apt-key add -
 sudo apt-add-repository 'deb [arch=amd64] https://repositories.intel.com/graphics/ubuntu focal main'
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
   intel-opencl-icd \
   intel-level-zero-gpu level-zero \
   intel-media-va-driver-non-free libmfx1
