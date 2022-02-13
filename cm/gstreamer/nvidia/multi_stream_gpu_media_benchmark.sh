4b52fdf22e076a03069f8a4d00fa3bcd0227c90b
ngboonhai <boon.hai.ng@intel.com>
2022-02-13 21:27:16 +0800

    Update multi_stream_gpu_media_benchmark.sh

--- cm/gstreamer/nvidia/multi_stream_gpu_media_benchmark.sh
+++ cm/gstreamer/nvidia/multi_stream_gpu_media_benchmark.sh
@@ -29,7 +29,7 @@ fi
 
 cmd="${SUDO} gst-launch-1.0 filesrc location=~/bbb_sunflower_2160p_60fps_normal.mp4 num-buffers=$TotalFrame ! qtdemux ! queue ! h264parse ! queue ! nvv4l2decoder ! queue ! videoconvert ! queue ! nvv4l2h264enc ! filesink location=sample_output_v4l2_h264.mp4 -e"
 log_filename="gst_v4l2_h264"
-#gstreamer_cmd=$cmd
+rm *$log_filename*.log
 
 for (( num=1; num <= $stream; num++))
 do
@@ -65,5 +65,3 @@ done
 echo "============================="
 echo "Total Throughtput of $num Stream: $Total_throughput" fps
 echo "============================="
-
-rm *$log_filename*.log
