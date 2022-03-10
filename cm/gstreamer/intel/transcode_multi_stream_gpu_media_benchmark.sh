383252ca8fd8f6f60ee59a8532277b033bbb2280
ngboonhai <boon.hai.ng@intel.com>
2022-03-10 18:49:51 +0800

    Update and rename multi_stream_gpu_media_benchmark.sh to transcode_multi_stream_gpu_media_benchmark.sh

--- cm/gstreamer/intel/transcode_multi_stream_gpu_media_benchmark.sh
+++ cm/gstreamer/intel/transcode_multi_stream_gpu_media_benchmark.sh
@@ -28,7 +28,7 @@ if [ "${SYSTEM_ARCH}" == "aarch64" ]; then
         SUDO="sudo"
 fi
 
-cmd="gst-launch-1.0 filesrc location=~/bbb_sunflower_2160p_60fps_normal.mp4 num-buffers=$TotalFrame ! qtdemux ! queue ! h264parse ! queue ! vaapih264dec ! queue ! vaapih264enc bitrate=8000 ! mp4mux ! filesink location=sample_output_vaapi_h264.mp4 -e"
+cmd="gst-launch-1.0 filesrc location=~/bbb_sunflower_2160p_60fps_normal.mp4 num-buffers=$TotalFrame ! qtdemux ! queue ! h264parse ! queue ! vaapih264dec ! queue ! vaapih264enc bitrate=8000 ! mp4mux ! filesink location=sample_output_transcode_vaapi_h264.mp4 -e"
 log_filename="gst_h264"
 rm *$log_filename*.log
 
@@ -47,18 +47,21 @@ done
 #echo $gstreamer_cmd
 eval $gstreamer_cmd
 sleep 10
+
+TotalFrameTranscode=`ffmpeg -i ~/sample_output_transcode_vaapi_h264.mp4 -vcodec copy -acodec copy -f null /dev/null 2>&1 | grep 'frame=' | sed 's/^.*\r/\r/' | awk '{print $2}' | grep -o '[0-9]\+'`
+
 echo " ==== Thoughput ==== "
 for (( num=1; num <= $stream; num++))
 do
 
         if [ $num -lt 2 ]; then
                 TotalTime_h264=$(grep "Execution ended" "$log_filename-$num.log" | awk '{print $4}' | awk -F: '{print ($1 * 3600) + ($2 * 60) + $3}' )
-                Throughput_h264=$(bc <<< "scale=2; $TotalFrame / $TotalTime_h264")
+                Throughput_h264=$(bc <<< "scale=2; $TotalFrameTranscode / $TotalTime_h264")
                 echo Stream $num: $Throughput_h264 fps
                 Total_throughput=$Throughput_h264
         else
                 TotalTime_h264=$(grep "Execution ended" "$log_filename-$num.log" | awk '{print $4}' | awk -F: '{print ($1 * 3600) + ($2 * 60) + $3}' )
-                Throughput_h264=$(bc <<< "scale=2; $TotalFrame / $TotalTime_h264")
+                Throughput_h264=$(bc <<< "scale=2; $TotalFrameTranscode / $TotalTime_h264")
                 echo Stream $num: $Throughput_h264 fps
                 Total_throughput=$(bc <<< "scale=2; $Total_throughput + $Throughput_h264")
         fi
