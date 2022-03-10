3169b5aeb682a2179766cae2f703d374684a40e9
ngboonhai <boon.hai.ng@intel.com>
2022-03-10 19:33:39 +0800

    Update transcode_multi_stream_gpu_media_benchmark.sh

--- cm/gstreamer/intel/transcode_multi_stream_gpu_media_benchmark.sh
+++ cm/gstreamer/intel/transcode_multi_stream_gpu_media_benchmark.sh
@@ -28,7 +28,7 @@ if [ "${SYSTEM_ARCH}" == "aarch64" ]; then
         SUDO="sudo"
 fi
 
-cmd="gst-launch-1.0 filesrc location=~/bbb_sunflower_2160p_60fps_normal.mp4 num-buffers=$TotalFrame ! qtdemux ! queue ! h264parse ! queue ! vaapih264dec ! queue ! vaapih264enc bitrate=8000 ! mp4mux ! filesink location=sample_output_transcode_vaapi_h264.mp4 -e"
+cmd="gst-launch-1.0 filesrc location=~/bbb_sunflower_2160p_60fps_normal.mp4 num-buffers=$TotalFrame ! qtdemux ! queue ! h264parse ! queue ! vaapih264dec ! queue ! vaapih264enc bitrate=8000 ! mp4mux ! perf ! filesink location=sample_output_transcode_vaapi_h264.mp4 -e"
 log_filename="gst_h264"
 rm *$log_filename*.log ~/sample_output*
 
