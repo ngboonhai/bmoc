980cadd0295dabb0e4f770f1f768c391120318da
ngboonhai <boon.hai.ng@intel.com>
2022-04-01 11:52:25 +0800

    Update multi_codec_transcode_gpu_media_benchmark.sh

--- cm/gstreamer/amd/multi_codec_transcode_gpu_media_benchmark.sh
+++ cm/gstreamer/amd/multi_codec_transcode_gpu_media_benchmark.sh
@@ -17,12 +17,12 @@ do
                 transcode_cmd="gst-launch-1.0 filesrc location=~/${video_src} num-buffers=$TotalFrame ! qtdemux ! queue ! ${code1}parse ! queue ! vaapi${code1}dec ! queue ! vaapi${code1}enc bitrate=8000 rate-control=cbr tune=high-compression ! queue ! perf ! fakesink -e"
         elif [ "$code1" == "h265" ]; then
                 video_src="bbb_sunflower_2160p_60fps_normal.mkv"
-                transcode_cmd="gst-launch-1.0 filesrc location=~/${video_src} num-buffers=$TotalFrame ! matroskademux ! queue ! ${code1}parse ! queue ! vaapi${code1}dec  ! queue ! vaapi${code1}enc bitrate=8000 tune=low-power low-delay-b=1 ! queue ! perf ! fakesink -e"
+                transcode_cmd="gst-launch-1.0 filesrc location=~/${video_src} num-buffers=$TotalFrame ! matroskademux ! queue ! ${code1}parse ! queue ! vaapi${code1}dec  ! queue ! vaapi${code1}enc bitrate=8000 low-delay-b=1 ! queue ! perf ! fakesink -e"
         elif [ "$code1" == "h264_265" ]; then
                 decode="h264"
                 encode="h265"
                 video_src="bbb_sunflower_2160p_60fps_normal.mp4"
-                transcode_cmd="gst-launch-1.0 filesrc location=~/${video_src} num-buffers=$TotalFrame ! qtdemux ! queue ! ${decode}parse ! queue ! vaapi${decode}dec  ! queue ! vaapi${encode}enc bitrate=8000 tune=low-power low-delay-b=1 ! queue ! perf ! fakesink -e"
+                transcode_cmd="gst-launch-1.0 filesrc location=~/${video_src} num-buffers=$TotalFrame ! qtdemux ! queue ! ${decode}parse ! queue ! vaapi${decode}dec  ! queue ! vaapi${encode}enc bitrate=8000 low-delay-b=1 ! queue ! perf ! fakesink -e"
         fi
 
         log_filename="transcode_gst_${code1}"
