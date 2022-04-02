0d98e64fadb70c1a281c643ac3cd8ef7d588d67c
ngboonhai <boon.hai.ng@intel.com>
2022-04-02 12:51:35 +0800

    Update msmc_encode_gpu_media_benchmark.sh

--- cm/gstreamer/nvidia/msmc_encode_gpu_media_benchmark.sh
+++ cm/gstreamer/nvidia/msmc_encode_gpu_media_benchmark.sh
@@ -25,10 +25,10 @@ do
 		encode_cmd="${SUDO} gst-launch-1.0 filesrc location=~/${video_src} ! videoparse width=4096 height=2160 format=nv12 framerate=60/1 ! queue ! ${VIDEO_CONVERTOR} ! queue ! nvv4l2${code1}enc ! queue ! perf ! fakesink -e"
 		gstreamer_encode_cmd="$encode_cmd > ${log_filename}_${code1}.log"
 		gstreamer_encode_multi_cmd="$gstreamer_encode_multi_cmd & $gstreamer_encode_cmd"
-	else
-		encode_cmd="${SUDO} gst-launch-1.0 filesrc location=~/${video_src} ! videoparse width=4096 height=2160 format=nv12 framerate=60/1 ! queue ! ${VIDEO_CONVERTOR} ! queue ! nvv4l2${code1}enc ! queue ! perf ! fakesink -e"
-		gstreamer_encode_cmd="$encode_cmd > ${log_filename}_${code1}.log"
-		gstreamer_encode_multi_cmd="$gstreamer_encode_multi_cmd & $gstreamer_encode_cmd"
+	#else
+	#	encode_cmd="${SUDO} gst-launch-1.0 filesrc location=~/${video_src} ! videoparse width=4096 height=2160 format=nv12 framerate=60/1 ! queue ! ${VIDEO_CONVERTOR} ! queue ! nvv4l2${code1}enc ! queue ! perf ! fakesink -e"
+	#	gstreamer_encode_cmd="$encode_cmd > ${log_filename}_${code1}.log"
+	#	gstreamer_encode_multi_cmd="$gstreamer_encode_multi_cmd & $gstreamer_encode_cmd"
 	fi
 done
 echo ''
